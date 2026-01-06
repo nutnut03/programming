a298c5b6effc1b252306a87a01d3cc3178152a2e
nutnut03 <carlo.meriseinc@gmail.com>
2026-01-06 16:56:22 +0800

    update code for view request

--- Php-Practice/MeRISE/view-requests.php
+++ Php-Practice/MeRISE/view-requests.php
@@ -67,6 +67,17 @@ $result = $conn->query("SELECT * FROM requests WHERE username='{$_SESSION['usern
             <?php endwhile; ?>
 
     </div>
+    <footer class="footer">
+        <div class="footer-container">
+            <h3>MeRISE English Academy</h3>
+            <p>ESY Building, Corner Juana Osmeña St., Brgy. Kamputhaw, Cebu City</p>
+            <p><strong>Tel. (PLDT):</strong> (032) 345 8524 <br><strong>Tel. (Globe):</strong> (032) 479 0414</p>
+            <p><strong>Email:</strong> <a href="mailto:academic_support@meriseinc.com">academic_support@meriseinc.com</a></p>
+        </div>
+        <div class="footer-bottom">
+            <p>© 2025 MeRISE English Academy. All rights reserved.</p>
+        </div>
+    </footer>
 
     <script>
         const rows = document.querySelectorAll(".requests-container table tr");
@@ -80,17 +91,7 @@ $result = $conn->query("SELECT * FROM requests WHERE username='{$_SESSION['usern
         }
     </script>
 
-    <footer class="footer">
-        <div class="footer-container">
-            <h3>MeRISE English Academy</h3>
-            <p>ESY Building, Corner Juana Osmeña St., Brgy. Kamputhaw, Cebu City</p>
-            <p><strong>Tel. (PLDT):</strong> (032) 345 8524 <br><strong>Tel. (Globe):</strong> (032) 479 0414</p>
-            <p><strong>Email:</strong> <a href="mailto:academic_support@meriseinc.com">academic_support@meriseinc.com</a></p>
-        </div>
-        <div class="footer-bottom">
-            <p>© 2025 MeRISE English Academy. All rights reserved.</p>
-        </div>
-    </footer>
+
 </body>
 
 </html>
